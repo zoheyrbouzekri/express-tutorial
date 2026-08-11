ac99fda5acc238fa1b48db5d786285767ea53965
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-11 22:48:56 +0100

    Route Parameters

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/", (req,res) => {
-    res.send('Hello from Express!')
-})
+const products = [
+    { id: 1, name: "Laptop", price: 200000 },
+    { id: 2, name: "Mouse", price: 7000 }
+];
 
-app.get("/about", (req,res) => {
-    res.send('This is the About Page')
-})
+app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+});
 
-app.get("/contact", (req,res) => {
-    res.send('This is the Contact Page')
-})
+app.get("/about", (req, res) => {
+    res.send("This is the About Page");
+});
 
-app.get("/products", (req,res) => {
-    res.send([
-        {id:1, name:"Laptop", price:200000},
-        {id:2, name:"Mouse", price: 7000}
-    ])
-})
+app.get("/contact", (req, res) => {
+    res.send("This is the Contact Page");
+});
+
+app.get("/products", (req, res) => {
+    res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const requestedProduct = products.find(
+        (product) => product.id === id
+    );
+
+    res.json(requestedProduct);
+});
 
 app.listen(3000, () => {
-    console.log("The server is running!")
-})
+    console.log("The server is running!");
+});
