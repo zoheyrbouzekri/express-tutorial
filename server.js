0e3ce3c99480ccfc0a781c91775483bd76c98a4f
Zoheyr BOUZEKRI <zoheyrbouzekri@gmail.com>
2026-08-11 22:33:40 +0100

    JSON

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get("/about", (req,res) => {
     res.send('This is the About Page')
 })
 
+app.get("/contact", (req,res) => {
+    res.send('This is the Contact Page')
+})
+
+app.get("/products", (req,res) => {
+    res.send([
+        {id:1, name:"Laptop", price:200000},
+        {id:2, name:"Mouse", price: 7000}
+    ])
+})
+
 app.listen(3000, () => {
     console.log("The server is running!")
 })
